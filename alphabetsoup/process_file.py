ba02c27d9b046b840663e938d3d12ab7c32ff9b0
Joel Berendzen <joel@generisbio.com>
2020-06-24 17:59:33 -0600

    pyproject.toml and deps

--- alphabetsoup/process_file.py
+++ alphabetsoup/process_file.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 "Process sequence file, using logging for asynchronous output"
+# standard library imports
 # stdlib imports
 import zlib
 from collections import defaultdict
 from operator import itemgetter
 
+# first-party imports
+import numpy as np
 from Bio import SeqIO
 from Bio.Data import IUPACData
 
-import numpy as np
+# module imports
+from .common import AMBIG_NAME
+from .common import DUP_NAME
+from .common import LENGTH_NAME
+from .common import SEQ_FILE_TYPE
+from .common import SHORT_NAME
+from .common import SUBSTRING_NAME
 
-from .common import AMBIG_NAME, DUP_NAME, LENGTH_NAME,\
-    SEQ_FILE_TYPE, SHORT_NAME, SUBSTRING_NAME
-
-ALPHABET = IUPACData.protein_letters + 'X' + '-'
-LOGINT_FMT = '%s\t%s\t%s\t%d'
-LOGFLOAT_FMT = '%s\t%s\t%s\t%f'
-LOGSTR_FMT = '%s\t%s\t%s\t%s'
+ALPHABET = IUPACData.protein_letters + "X" + "-"
+LOGINT_FMT = "%s\t%s\t%s\t%d"
+LOGFLOAT_FMT = "%s\t%s\t%s\t%f"
+LOGSTR_FMT = "%s\t%s\t%s\t%s"
 
 
 #
@@ -48,11 +54,7 @@ class ShortCounter(object):
         if len(s) < self.minlen:
             self.count += 1
             if self.log:
-                self.log.debug(LOGINT_FMT,
-                               self.name,
-                               id,
-                               SHORT_NAME,
-                               len(s))
+                self.log.debug(LOGINT_FMT, self.name, id, SHORT_NAME, len(s))
             return True
         else:
             return False
@@ -81,37 +83,40 @@ class AmbigCounter(object):
         :param id: ID of sequence for log
         :return: True if s is above fraction ambiguous
         """
-        ambig = sum([i == 'X' for i in s])
+        ambig = sum([i == "X" for i in s])
         if ambig > self.frac:
             self.count += 1
             if self.log:
-                self.log.debug(LOGFLOAT_FMT,
-                               self.name,
-                               id,
-                               AMBIG_NAME,
-                               ambig * 100. / len(s))
+                self.log.debug(
+                    LOGFLOAT_FMT,
+                    self.name,
+                    id,
+                    AMBIG_NAME,
+                    ambig * 100.0 / len(s),
+                )
             return True
         else:
             return False
 
 
+class DuplicateIDDict(defaultdict):
+    """A dictionary of lists with a get() that returns the surviving ID
+    """
+
+    def __init__(self, concat=False):
+        self.concat = concat
+        super().__init__(list)
+
+    def get(self, k):
+        if self.concat:
+            return "|".join([k] + self[k])
+        else:
+            return k
+
+
 class DuplicateCounter(object):
     """count sequences too short
     """
-
-    class DuplicateIDDict(defaultdict):
-        """A dictionary of lists with a get() that returns the surviving ID
-        """
-
-        def __init__(self, concat=False):
-            self.concat = concat
-            super().__init__(list)
-
-        def get(self, k):
-            if self.concat:
-                return '|'.join([k] + self[k])
-            else:
-                return k
 
     def __init__(self, log=False, name=None, concat_names=False):
         self.exact_count = 0
@@ -119,11 +124,11 @@ class DuplicateCounter(object):
         self.log = log
         self.name = name
         self.hash_dict = {}
-        self.dupDict = self.DuplicateIDDict(concat=concat_names)
+        self.dupDict = DuplicateIDDict(concat=concat_names)
 
     def exact(self, s, id):
         "test and count if exact duplicate"
-        seq_hash = zlib.adler32(bytearray(str(s), 'utf-8'))
+        seq_hash = zlib.adler32(bytearray(str(s), "utf-8"))
         if seq_hash not in self.hash_dict:
             self.hash_dict[seq_hash] = id
             return False
@@ -132,16 +137,11 @@ class DuplicateCounter(object):
             first_ID = self.hash_dict[seq_hash]
             self.dupDict[first_ID].append(id)
             if self.log:
-                self.log.debug(LOGSTR_FMT,
-                               self.name,
-                               id,
-                               DUP_NAME,
-                               first_ID)
+                self.log.debug(LOGSTR_FMT, self.name, id, DUP_NAME, first_ID)
             return True
 
     def index_by_length(self, items):
-        length_idx = [(i, len(item))
-                      for i, item in enumerate(items)]
+        length_idx = [(i, len(item)) for i, item in enumerate(items)]
         length_idx.sort(key=itemgetter(1))
         return [idx for idx, length in length_idx]
 
@@ -153,24 +153,27 @@ class DuplicateCounter(object):
             test_seq = seqs[idx].seq
             test_id = seqs[idx].id
             # traverse from biggest to smallest to find the biggest match
-            for record in [seqs[i] for i in
-                           reversed(ascending[item_num+1:])]:
+            for record in [
+                seqs[i] for i in reversed(ascending[item_num + 1 :])
+            ]:
                 if str(test_seq) in str(record.seq):
                     self.substring_count += 1
                     self.dupDict[record.id].append(test_id)
                     removal_list.append(idx)
                     if self.log:
-                        self.log.debug(LOGSTR_FMT,
-                                       self.name,
-                                       test_id,
-                                       SUBSTRING_NAME,
-                                       record.id)
+                        self.log.debug(
+                            LOGSTR_FMT,
+                            self.name,
+                            test_id,
+                            SUBSTRING_NAME,
+                            record.id,
+                        )
                     break
         # Optionally remove exact substring matches
         if remove and len(removal_list) > 0:
             removal_list.sort()
             for item_num, idx in enumerate(removal_list):
-                seqs.pop(idx-item_num)
+                seqs.pop(idx - item_num)
         return seqs
 
 
@@ -207,10 +210,11 @@ class Sanitizer(object):
         :param s: mutable sequence, upper-cased
         :return: fixed sequence
         """
-        fix_positions = [pos for pos, char in enumerate(s)
-                         if char not in ALPHABET]
+        fix_positions = [
+            pos for pos, char in enumerate(s) if char not in ALPHABET
+        ]
         self.chars_fixed = len(fix_positions)
-        [s.__setitem__(pos, 'X') for pos in fix_positions]
+        [s.__setitem__(pos, "X") for pos in fix_positions]
         return s
 
     def remove_char_on_ends(self, s, character):
@@ -236,24 +240,26 @@ class Sanitizer(object):
         self.seqs_sanitized += 1
         self.chars_in += len(s)
         if len(s) and self.remove_dashes:
-            s = self.char_remover(s, '-')
+            s = self.char_remover(s, "-")
         if len(s):
             s = self.fix_alphabet(s)
         if len(s):
-            s = self.remove_char_on_ends(s, 'X')
+            s = self.remove_char_on_ends(s, "X")
         return s
 
 
-def process_file(file,
-                 logger=None,
-                 write=False,
-                 min_len=0,
-                 min_seqs=0,
-                 max_ambiguous=0,
-                 remove_dashes=False,
-                 remove_dups=False,
-                 remove_substrings=False,
-                 lengths=False):
+def process_file(
+    file,
+    logger=None,
+    write=False,
+    min_len=0,
+    min_seqs=0,
+    max_ambiguous=0,
+    remove_dashes=False,
+    remove_dups=False,
+    remove_substrings=False,
+    lengths=False,
+):
     """fix alphabet and other problems in FASTA file
 
     :param file: path to sequence file
@@ -269,18 +275,13 @@ def process_file(file,
     :return:
     """
     if logger:
-        logger.debug('processing %s', file)
+        logger.debug("processing %s", file)
     out_sequences = []
     sanitizer = Sanitizer(remove_dashes=remove_dashes)
-    too_short = ShortCounter(length=min_len,
-                             log=logger,
-                             name=file.name)
-    too_ambig = AmbigCounter(frac=max_ambiguous,
-                             log=logger,
-                             name=file.name)
-    duplicated = DuplicateCounter(log=logger,
-                                  name=file.name)
-    with file.open('rU') as handle:
+    too_short = ShortCounter(length=min_len, log=logger, name=file.name)
+    too_ambig = AmbigCounter(frac=max_ambiguous, log=logger, name=file.name)
+    duplicated = DuplicateCounter(log=logger, name=file.name)
+    with file.open("rU") as handle:
         for record in SeqIO.parse(handle, SEQ_FILE_TYPE):
             seq = record.seq.upper().tomutable()
             seq = sanitizer.sanitize(seq)
@@ -302,12 +303,11 @@ def process_file(file,
     if remove_dups and remove_substrings:
         out_sequences = duplicated.substring(out_sequences)
     # Indicate (in ID) records deduplicated
-    if (remove_dups or remove_substrings) \
-            and len(duplicated.dupDict) > 0:
+    if (remove_dups or remove_substrings) and len(duplicated.dupDict) > 0:
         for record in out_sequences:
             if record.id in duplicated.dupDict:
                 record.id = duplicated.dupDict.get(record.id)
-                record.description = ''
+                record.description = ""
     # Check if file has enough sequences
     if len(out_sequences) < min_seqs:
         small = 1
@@ -317,13 +317,15 @@ def process_file(file,
         small = 0
         if lengths and logger:
             for record in out_sequences:
-                logger.debug(LOGINT_FMT,
-                             file.name,
-                             record.id,
-                             LENGTH_NAME,
-                             len(record.seq))
+                logger.debug(
+                    LOGINT_FMT,
+                    file.name,
+                    record.id,
+                    LENGTH_NAME,
+                    len(record.seq),
+                )
         if write:
-            with file.open('w') as output_handle:
+            with file.open("w") as output_handle:
                 SeqIO.write(out_sequences, output_handle, SEQ_FILE_TYPE)
     lengths_arr = np.array([len(s) for s in out_sequences])
     length_std = lengths_arr.std()
@@ -331,22 +333,24 @@ def process_file(file,
     if np.isnan(length_std):
         length_std_percent = 0
     else:
-        length_std_percent = int(round(length_std*100/length_mean))
+        length_std_percent = int(round(length_std * 100 / length_mean))
 
-    return (file.name,
-            # character stats
-            sanitizer.chars_in,
-            lengths_arr.sum(),
-            sanitizer.chars_removed,
-            sanitizer.chars_fixed,
-            sanitizer.endchars_removed,
-            # sequence stats
-            sanitizer.seqs_sanitized,
-            len(out_sequences),
-            too_ambig.count,
-            too_short.count,
-            duplicated.exact_count,
-            duplicated.substring_count,
-            # file stats
-            length_std_percent,
-            small)
+    return (
+        file.name,
+        # character stats
+        sanitizer.chars_in,
+        lengths_arr.sum(),
+        sanitizer.chars_removed,
+        sanitizer.chars_fixed,
+        sanitizer.endchars_removed,
+        # sequence stats
+        sanitizer.seqs_sanitized,
+        len(out_sequences),
+        too_ambig.count,
+        too_short.count,
+        duplicated.exact_count,
+        duplicated.substring_count,
+        # file stats
+        length_std_percent,
+        small,
+    )
